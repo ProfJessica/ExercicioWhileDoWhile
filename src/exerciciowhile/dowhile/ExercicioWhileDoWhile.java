19a277d914d560a0e1c94aff8ec4266ee3eceefe
ProfJessica <jessica.sgarcez@lideres.alicerceedu.com.br>
2021-08-28 14:45:10 -0300

    primeiro commit

---
+++ src/exerciciowhile/dowhile/ExercicioWhileDoWhile.java
@@ -0,0 +1,16 @@
+package exerciciowhile.dowhile;
+
+public class ExercicioWhileDoWhile {
+
+//1) Implemente em Java, um código que inicia uma variável ‘contador’
+// com o valor 0 e incrementa o valor enquanto a condição for satisfeita.
+// Imprima o número de repetições. Utilize a condição (contador < 10).
+
+    
+//2) Implemente em Java um programa que incrementa uma variável e imprima de 0 a 9.
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
